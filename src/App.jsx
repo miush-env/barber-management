3ac498057b53f5304cc40cccd07a89e318ef5957
Bau_ <basach1582@gmail.com>
2026-03-05 10:19:48 -0300

    Eliminación del encabezado y ajuste de la estructura del componente App para mostrar solo la página de inicio de sesión.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './styles/index.css'
+import SignIn from './pages/SignIn'
 
 function App() {
 	return (
 		<>
-			<div className='bg-gray-800 h-screen'>
-				<h1 className='text-2xl text-white text-center uppercase p-10'>barber management</h1>
+			<div className='h-screen'>
+				<SignIn />
 			</div>
 		</>
 	)
