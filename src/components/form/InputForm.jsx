5f40c1a35290ce9142acb844412fe6d706e2da07
Bau_ <basach1582@gmail.com>
2026-03-05 18:32:31 -0300

    mejorar el formulario de entrada: agregar requisitos y estilos al campo de contraseña

--- src/components/form/InputForm.jsx
+++ src/components/form/InputForm.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
 
-function InputForm({type, icon, visibility, placeholder}) {
+function InputForm({type, visibility, placeholder, required}) {
   const [showPassword, setShowPassword] = useState(false);
   const inputType = visibility && showPassword ? 'text' : type
 
   return (
-		<label htmlFor='' className='block text-sm font-medium text-gray-700'>
-			{icon}
-			<input type={inputType} placeholder={placeholder} />
+		<label
+			htmlFor=''
+			className='relative flex w-full rounded-xl text-slate-500 focus:ring-primary/20 border-slate-200  bg-slate-200 h-14 transition-all'
+		>
+			<input
+				className=' h-full w-full invalid:border-red-500 pl-2 pr-11 outline-0  focus:text-slate-900 placeholder:text-slate-500 valid:border valid:border-green-500 border-2 rounded-xl'
+				type={inputType}
+				placeholder={placeholder}
+				required={required}
+				minLength={type === 'password' ? 8 : 3}
+			/>
 			{visibility && (
 				<button
 					type='button'
 					onClick={() => setShowPassword(!showPassword)}
-					className='text-sm text-gray-500'
+					className=' fill-gray-500 absolute right-3 top-1/2 -translate-y-1/2'
 				>
-					{showPassword ? 'Ocultar' : 'Ver'}
+					<img
+						src={
+							showPassword
+								? '/src/assets/eye.svg'
+								: '/src/assets/eye-closed.svg'
+						}
+						alt=''
+					/>
 				</button>
 			)}
 		</label>
