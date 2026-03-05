56f8774db787278b52fbcc6c92ce2c65455b1c98
Bau_ <basach1582@gmail.com>
2026-03-05 11:30:27 -0300

    corregí algunos estilos mínimos

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,20 +1,22 @@
 import InputForm from "../components/form/InputForm";
+import SocialAuth from "../components/form/SocialAuth";
 
 function SignIn() {
   return (
-		<div className=''>
+		<div className='w-screen p-10'>
 			<div>
 				<h1 className='text-2xl '>Registrarme</h1>
-        <p className='text-gray-800'>Crea tu cuenta para continuar</p>
+				<p className='text-gray-800'>Crea tu cuenta para continuar</p>
 			</div>
-			<form action=''>
+			<form action='' className='flex flex-col'>
 				<InputForm type='text' placeholder='Nombre' />
 				<InputForm type='text' placeholder='Apellido' />
 				<InputForm type='email' placeholder='Correo electrónico' />
 				<InputForm type='password' placeholder='Contraseña' visibility={true} />
-				<button className='bg-gray-700 text-white w-full py-2 rounded mt-4'>
+				<button className='bg-blue-800 text-white w-full py-2 rounded mt-4'>
 					Registrarme
 				</button>
+				<SocialAuth name='Google' icon='Google' />
 			</form>
 		</div>
 	)
