51d12bb0bdb2c70a1c7044578153f882fab31f30
Bau_ <basach1582@gmail.com>
2026-03-06 20:55:03 -0300

    Aquí irán todos los componentes del panel principal del barbero

--- src/pages/PanelBarber.jsx
+++ src/pages/PanelBarber.jsx
@@ -1,4 +1,5 @@
 import NavBar from "../components/panelBarber/NavBar"
+import NextAppointment from "../components/panelBarber/NextAppointment"
 
 function PanelBarber() {
 return (
@@ -6,6 +7,9 @@ return (
       <header>
         <NavBar name="John Doe" photo="https://i.pinimg.com/736x/4a/d1/13/4ad113eaace2e06b92f78dc15c1cf8de.jpg" notifications={5} />
       </header>
+      <article>
+        <NextAppointment />
+      </article>
     </main>
 )
 }
