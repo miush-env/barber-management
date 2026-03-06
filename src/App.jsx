6e166190c48c707f37d83f38acddeaad0aefe013
Bau_ <basach1582@gmail.com>
2026-03-06 11:20:07 -0300

    Creación del panel para el barbero

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './styles/index.css'
 import SignIn from './pages/SignIn'
 import Login from './pages/Login'
+import PanelBarber from './pages/PanelBarber'
 
 function App() {
 	return (
 		<>
 			<div>
 				{/* <SignIn /> */}
-				<Login />
+				{/* <Login /> */}
+				<PanelBarber />
 			</div>
 		</>
 	)
