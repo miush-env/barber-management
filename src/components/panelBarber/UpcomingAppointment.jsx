a459f832f652b5568c8bf2b1c909e7597233d4ca
Bau_ <basach1582@gmail.com>
2026-03-07 11:47:05 -0300

    ejemplo real de como se verían las cards

--- src/components/panelBarber/UpcomingAppointment.jsx
+++ src/components/panelBarber/UpcomingAppointment.jsx
@@ -4,8 +4,25 @@ function UpcomingAppointment() {
   return (
 		<section className='p-6 flex flex-col gap-2'>
 			<h2 className='text-2xl text-gray-700 font-bold'>Próximas Citas</h2>
-			<div>
-        <CardUpcomingAppointment />
+			<div className="flex flex-col gap-4">
+				<CardUpcomingAppointment
+					clientName='Juan Ramirez'
+					time='11:00'
+					service='Corte Clásico'
+					status={true}
+				/>
+				<CardUpcomingAppointment
+					clientName='Mateo Vazquez'
+					time='12:00'
+					service='Degradado + Barba'
+					status={true}
+				/>
+				<CardUpcomingAppointment
+					clientName='Ariel Dundo'
+					time='13:30'
+					service='Corte Clásico'
+					status={false}
+				/>
 			</div>
 		</section>
 	)
