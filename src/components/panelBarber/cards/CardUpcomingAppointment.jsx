0158bafa90b39d9b494fe2490ddbf77b6d7b6c69
Bau_ <basach1582@gmail.com>
2026-03-07 11:47:46 -0300

    correction en el color del texto porque hay mucho contenido visual o llamativo

--- src/components/panelBarber/cards/CardUpcomingAppointment.jsx
+++ src/components/panelBarber/cards/CardUpcomingAppointment.jsx
@@ -8,10 +8,10 @@ function CardUpcomingAppointment({time, clientName = 'Sin nombre', service = "co
 			className={`relative w-full h-18 px-4 bg-white  rounded-xl border-2 ${status ? 'border-green-500' : 'border-red-500'} flex items-center gap-4`}
 		>
 			<div className='flex flex-col text-center items-center justify-center'>
-				<span className='font-bold uppercase text-base text-black'>{time}</span>
+				<span className='font-bold uppercase text-base text-gray-600'>{time}</span>
 			</div>
 			<div className='w-full'>
-				<h3 className='text-lg text-black font-bold'>{clientName}</h3>
+				<h3 className='text-lg text-gray-600 font-bold'>{clientName}</h3>
 				<span className='text-gray-500 text-sm font-semibold'>{service}</span>
 				<img
 					src='/src/assets/arrow-up-right.svg'
