5addfcf73c0b7de8e8f651e066fb7aeef3944510
Bau_ <basach1582@gmail.com>
2026-03-07 09:31:12 -0300

    utilize el componente para ver como se vería en uso y los datos que pedirá al backEnd

--- src/components/panelBarber/NextAppointment.jsx
+++ src/components/panelBarber/NextAppointment.jsx
@@ -3,9 +3,17 @@ import CardNextAppointment from './cards/CardNextAppointment'
 function NextAppointment() {
   return (
 		<section className='p-6 flex flex-col gap-2'>
-			<h2 className="text-2xl text-gray-700 font-bold">Cita Actual</h2>
+			<h2 className='text-2xl text-gray-700 font-bold'>Cita Actual</h2>
 			<div>
-				<CardNextAppointment />
+			
+				<CardNextAppointment
+					nameClient='Julian Ramirez'
+					status='confirmada'
+					time='10:30'
+					serviceName='Corte clásico + barba'
+					timeService='45 min'
+					price='8000'
+				/>
 			</div>
 		</section>
 	)
