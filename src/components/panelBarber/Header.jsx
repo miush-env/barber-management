087088d528464fd8a75e22f0440eb3213f09d008
Bau_ <basach1582@gmail.com>
2026-03-10 21:00:41 -0300

    correcciones mínimas en el Header

---
+++ src/components/panelBarber/Header.jsx
@@ -0,0 +1,34 @@
+function Header ({name, photo, notifications}) {
+  return (
+		<header className='flex w-full items-center justify-between px-6 h-24 bg-white border-b-2 border-b-gray-300 shadow-lg shadow-white text-white'>
+			<div className='flex gap-2 items-center'>
+				<img
+					src={photo}
+					alt={name}
+					className='rounded-full w-14 border-2 border-gray-400'
+				/>
+				<div className=''>
+					<span className='text-sm text-gray-600 font-semibold'>
+						07 vie Mar 2026
+					</span>
+					<h1 className='text-3xl text-black font-bold'>{name}</h1>
+				</div>
+			</div>
+			<div className='relative'>
+				<span
+					className=' absolute -top-1 -right-1
+    flex items-center justify-center
+    w-5 h-5
+    text-xs font-semibold
+    text-white bg-red-600
+    rounded-full'
+				>
+					{notifications}
+				</span>
+				<img src='/src/assets/bell.svg' alt='icon bell' className='w-7' />
+			</div>
+		</header>
+	)
+}
+
+export default Header
