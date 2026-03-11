2669efd59def155ca325c07e342daae4fb46a2cc
Bau_ <basach1582@gmail.com>
2026-03-10 21:43:56 -0300

    Cambios en el icono por temas de rapidez y resultado mas rápido y finalización del componente

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,58 @@
 function NavBar() {
-  return (
-		<nav className='flex *:text-black [&>a]:flex [&>a]:flex-col [&>a]:justify-center [&>a]:items-center [&>a>img]:w-6  w-full items-center px-6 h-14 justify-evenly bg-white border-b-2 border-t-gray-300 shadow-lg shadow-white text-white'>
-			<a href='#' className=''>
-				<img src='/src/assets/home.svg' alt='inicio' />
+	const active = 'clientes' // luego esto vendrá del router
+
+	return (
+		<nav className='fixed bottom-0 *:font-bold left-0 w-full h-16 bg-white border-t border-gray-200 flex justify-around items-center'>
+			<a
+				href='#'
+				className={`flex flex-col items-center text-xs ${
+					active === 'inicio' ? 'text-slate-900' : 'text-gray-400'
+				}`}
+			>
+				<img src='/src/assets/home.svg' className='w-6 mb-1' />
 				<span>Inicio</span>
+				{active === 'inicio' && (
+					<div className='w-6 h-[2px] bg-slate-900 mt-1 rounded'></div>
+				)}
 			</a>
-			<a href='#'>
-				<img src='/src/assets/calendar-week.svg' alt='calendario' />
-				<span>Citas</span>
+
+			<a
+				href='#'
+				className={`flex flex-col items-center text-xs ${
+					active === 'agenda' ? 'text-slate-900' : 'text-gray-400'
+				}`}
+			>
+				<img src='/src/assets/calendar-week.svg' className='w-6 mb-1' />
+				<span>Agenda</span>
+				{active === 'agenda' && (
+					<div className='w-6 h-[2px] bg-slate-900 mt-1 rounded'></div>
+				)}
 			</a>
-			<a href='#'>
-				<img src='/src/assets/users-group.svg' alt='clients' />
+
+			<a
+				href='#'
+				className={`flex flex-col items-center text-xs ${
+					active === 'clientes' ? 'text-slate-900' : 'text-gray-400'
+				}`}
+			>
+				<img src='/src/assets/users-group-nav.svg' className='w-6 mb-1' />
 				<span>Clientes</span>
+				{active === 'clientes' && (
+					<div className='w-6 h-[2px] bg-slate-900 mt-1 rounded'></div>
+				)}
 			</a>
-			<a href='#'>
-				<img src='/src/assets/user.svg' alt='perfil' />
+
+			<a
+				href='#'
+				className={`flex flex-col items-center text-xs ${
+					active === 'perfil' ? 'text-slate-900' : 'text-gray-400'
+				}`}
+			>
+				<img src='/src/assets/user.svg' className='w-6 mb-1' />
 				<span>Perfil</span>
+				{active === 'perfil' && (
+					<div className='w-6 h-[2px] bg-slate-900 mt-1 rounded'></div>
+				)}
 			</a>
 		</nav>
 	)
