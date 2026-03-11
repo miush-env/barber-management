c1e7519d5c2dedc3551c5da900b66d3a91fa744d
Bau_ <basach1582@gmail.com>
2026-03-10 21:45:09 -0300

    ChatGpt corrigió el tema del espaciado para que el navBar este posicionado en la parte inferior de la pantalla

--- src/pages/PanelBarber.jsx
+++ src/pages/PanelBarber.jsx
@@ -7,19 +7,23 @@ import NavBar from '../components/NavBar'
 
 function PanelBarber() {
 	return (
-		<main className='bg-gray-100 h-screen'>
+		<main className='bg-gray-100 min-h-screen flex flex-col'>
 			<Header
 				name='John Doe'
 				photo='https://i.pinimg.com/736x/4a/d1/13/4ad113eaace2e06b92f78dc15c1cf8de.jpg'
 				notifications={5}
-
 			/>
-			<NextAppointment />
-			<UpcomingAppointment />
-			<article className='flex justify-between p-6 gap-6'>
-				<CardData title='Clientes' value='4/8' style='clients' />
-				<CardData title='Ganancia' value='24300' style='earnings' />
-			</article>
+
+			<div className='flex-1'>
+				<NextAppointment />
+				<UpcomingAppointment />
+
+				<article className='flex justify-between p-6 gap-6'>
+					<CardData title='Clientes' value='4/8' style='clients' />
+					<CardData title='Ganancia' value='24300' style='earnings' />
+				</article>
+			</div>
+
 			<footer>
 				<NavBar />
 			</footer>
