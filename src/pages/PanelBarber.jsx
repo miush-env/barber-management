88bb7b7d635db99abb942803a4211308e0b124ed
Bau_ <basach1582@gmail.com>
2026-03-11 22:33:39 -0300

    correcciones sugeridas por chatGPT para que este bien visualmente

--- src/pages/PanelBarber.jsx
+++ src/pages/PanelBarber.jsx
@@ -4,17 +4,16 @@ import UpcomingAppointment from '../components/panelBarber/UpcomingAppointment'
 import CardData from '../components/panelBarber/cards/CardData'
 import NavBar from '../components/NavBar'
 
-
 function PanelBarber() {
 	return (
-		<main className='bg-gray-100 min-h-screen flex flex-col relative'>
+		<main className='bg-gray-100 flex flex-col relative min-h-screen pb-20'>
 			<Header
 				name='John Doe'
 				photo='https://i.pinimg.com/736x/4a/d1/13/4ad113eaace2e06b92f78dc15c1cf8de.jpg'
 				notifications={5}
 			/>
 
-			<div className='flex-1'>
+			<div className=''>
 				<NextAppointment />
 				<UpcomingAppointment />
 
@@ -24,16 +23,15 @@ function PanelBarber() {
 				</article>
 			</div>
 
-			<footer>
-				<NavBar />
-			</footer>
+			<NavBar />
 
-			<a
+			<button
 				href='#'
-				className='absolute right-3 bottom-15 bg-linear-to-t from-green-500 to-green-600 shadow-lg shadow-green-300 rounded-full p-1'
+				className='fixed right-3 bottom-15 bg-linear-to-t from-green-500 to-green-600 shadow-lg active:to-green-500/90 
+				active:from-green-600/90 shadow-green-300 rounded-full p-1 transition-colors duration-300 '
 			>
 				<img src='/src/assets/plus.svg' alt='crear cita' className='w-9' />
-			</a>
+			</button>
 		</main>
 	)
 }
