3939d9fd0742939f7093608c89902b29874e3ef8
Bau_ <basach1582@gmail.com>
2026-03-11 22:29:25 -0300

    configure correctamente los path me costo mucho y chatGPT muchas vueltas

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+
 	resolve: {
 		alias: {
-      '@assets': './src/assets',
-      '@components': './src/components',
-      '@pages': './src/pages',
-    },
+			'@components': path.resolve(__dirname, './src/components'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@styles': path.resolve(__dirname, './src/styles'),
+		},
 	},
 })
