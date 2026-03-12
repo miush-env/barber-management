a3fa97c28d00e4ee7d5e70077c5bdc191801aa59
Bau_ <basach1582@gmail.com>
2026-03-12 16:36:30 -0300

    configure react router y cual sera la pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './styles/index.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import SignIn from './pages/SignIn'
 import Login from './pages/Login'
 import PanelBarber from './pages/PanelBarber'
+import CreateAppointment from './pages/CreateAppointment.jsx'
 
 function App() {
 	return (
-		<>
-			<div>
-				{/* <SignIn /> */}
-				{/* <Login /> */}
-				<PanelBarber />
-			</div>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' index element={<Login />} />
+				<Route path='/sign-in' element={<SignIn />} />
+
+				<Route path='/Crear-cita' element={<CreateAppointment />} />
+				<Route path='/home' element={<PanelBarber />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
