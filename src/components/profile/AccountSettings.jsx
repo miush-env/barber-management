41d05439ddf0852e721fe3d12edb0117495f0861
Bau_ <basach1582@gmail.com>
2026-03-15 19:56:20 -0300

    Termine los estilos bases del componente por ahora es solo el botón de cerrar sesion pero se espera mas utilidades

---
+++ src/components/profile/AccountSettings.jsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router'
+
+function AccountSettings() {
+  const navigate = useNavigate()
+
+  return (
+		<section>
+			<div className='flex gap-2 bg-white p-2 rounded-lg items-center'>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					width='24'
+					height='24'
+					viewBox='0 0 24 24'
+					fill='none'
+					stroke='currentColor'
+					stroke-width='2'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+					className='stroke-red-500'
+				>
+					<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+					<path d='M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2' />
+					<path d='M9 12h12l-3 -3' />
+					<path d='M18 15l3 -3' />
+				</svg>
+				<span className='uppercase text-red-500 font-bold text-sm' onClick={()=>{	navigate('/')}}>Cerrar Sesion</span>
+			</div>
+		</section>
+	)
+}
+
+export default AccountSettings
