b8a76a6ef35f80584abf6e6803f0f0894f2643fa
Bau_ <basach1582@gmail.com>
2026-03-19 21:02:20 -0300

    componente encargado de modificar la fecha de cada cita funcionando perfectamente

---
+++ src/components/CreateAppointment/Cal.com/RescheduleIframe.jsx
@@ -0,0 +1,17 @@
+export default function RescheduleIframe({
+	calUsername,
+	eventSlug,
+	rescheduleUid,
+}) {
+	const src = `https://cal.com/${calUsername}/${eventSlug}?rescheduleUid=${rescheduleUid}`
+
+	return (
+		<iframe
+			src={src}
+			width='100%'
+			height='700px'
+			frameBorder='0'
+			style={{ border: 'none' }}
+		/>
+	)
+}
