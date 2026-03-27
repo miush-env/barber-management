9c102aed78b40813c5553f9550ca5cc230d5024c
Bau_ <basach1582@gmail.com>
2026-03-27 18:21:09 -0300

    modifique los estilos de los botones para que sean mas sencillos y sin tanto impacto visual

--- src/components/form/SocialAuth.jsx
+++ src/components/form/SocialAuth.jsx
@@ -1,14 +1,11 @@
 function SocialAuth({name, icon}) {
   return (
-		<button className='flex px-2 w-full gap-2 group items-center active:bg-gray-200/45 rounded-xl h-14 transition-colors duration-300 '>
+		<button className='flex items-center justify-center rounded-lg p-2 shadow-md shadow-gray-400 bg-gray-100 w-12 h-12'>
 			<img
 				src={`./src/assets/${icon}.svg`}
 				alt={name}
-				className='w-6'
+				className='w-6 h-6'
 			/>
-			<span className='text-lg font-bold text-gray-700 group-active:text-gray-500 bg-transparent transition-colors duration-300'>
-				Iniciar con {name}
-			</span>
 		</button>
 	)
 }
