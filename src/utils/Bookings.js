5f894f5ba48ee069802b12a1b086b04869069df7
Bau_ <basach1582@gmail.com>
2026-03-29 18:59:05 -0300

    movi las peticiones a de las citas para que no haya mucho código en viewAppointments y para facilitar su uso a futuro

--- src/utils/Bookings.js
+++ src/utils/Bookings.js
@@ -1,3 +1,5 @@
+const API_KEY = import.meta.env.VITE_API_SECRET
+
 export const getTodayBookings = async () => {
 		const today = new Date()
 		const startOfDay = new Date(
@@ -17,7 +19,7 @@ export const getTodayBookings = async () => {
 			method: 'GET',
 			headers: {
 				//  cal_live_c144e9d89987946024fa42e93413a28b
-				Authorization: `Bearer ${process.env.CAL_API_SECRET}`,
+				Authorization: `Bearer ${API_KEY}`,
 				'cal-api-version': '2026-02-25',
 			},
 		})
@@ -25,4 +27,61 @@ export const getTodayBookings = async () => {
 		const data = await response.json()
 		console.log(data.data)
 		return data.data
+}
+
+export const getNameEvent = async () => {
+	try {
+		const response = await fetch('https://api.cal.com/v2/event-types', {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${API_KEY}`,
+				'cal-api-version': '2024-08-14',
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error: ${response.status}`);
+		}
+
+		const data = await response.json();
+
+		const events =
+			data?.data?.eventTypeGroups?.[0]?.eventTypes || [];
+
+		return events;
+
+	} catch (error) {
+		console.error('Error al obtener eventos:', error);
+		return []; // importante devolver algo consistente
 	}
+};
+
+export const bookingEvent = async () => {
+	try {
+		const response = await fetch(
+			'https://api.cal.com/v2/bookings?status=upcoming',
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${API_KEY}`,
+					'cal-api-version': '2026-02-25',
+					'Content-Type': 'application/json',
+				},
+			},
+		)
+
+		if (!response.ok) {
+			throw new Error(`HTTP error: ${response.status}`)
+		}
+
+		const data = await response.json()
+		return data.data		
+		// console.log('Citas obtenidas:', data.data)
+		// setAppointments(data.data)
+
+		
+	} catch (error) {
+		console.log('Error: ', error)
+	}
+}
