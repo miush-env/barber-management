087a59a42d26d95bbca453a643278dd49da372c9
Bau_ <basach1582@gmail.com>
2026-03-29 19:29:18 -0300

    moví todo el condigo encargo de abrir la modal a un archivo propio

--- src/components/CreateAppointment/Cal.com/Calendar.jsx
+++ src/components/CreateAppointment/Cal.com/Calendar.jsx
@@ -1,78 +1,32 @@
-/* First make sure that you have installed the package */
+import { serviceCorte, serviceCorteBarba, serviceSoloPuntas, serviceGlobal } from '../../../utils/ServiceCal'
 
-/* If you are using yarn */
-// yarn add @calcom/embed-react
-
-/* If you are using npm */
-// npm install @calcom/embed-react
-
-import { getCalApi } from '@calcom/embed-react'
+// import { getCalApi } from '@calcom/embed-react'
 export default function Calendar() {
-	const openCorte = async () => {
-		const cal = await getCalApi({ namespace: 'corte-clasico' })
-		cal('modal', {
-			calLink: 'multipurpose-ki7ln0/corte-clasico',
-			config: {
-				layout: 'month_view',
-			},
-		})
-	}
-
-	const openCorteBarba = async () => {
-		const cal = await getCalApi({ namespace: 'corte-clasico-barba' })
-		cal('modal', {
-			calLink: 'multipurpose-ki7ln0/corte-clasico-barba',
-			config: {
-				layout: 'month_view',
-			},
-		})
-	}
-
-	const openGlobal = async () => {
-		const cal = await getCalApi({ namespace: 'global' })
-		cal('modal', {
-			calLink: 'multipurpose-ki7ln0/global',
-			config: {
-				layout: 'month_view',
-			},
-		})
-	}
-
-	const openSoloPuntas = async () => {
-		const cal = await getCalApi({ namespace: 'tinte-solo-puntas' })
-		cal('modal', {
-			calLink: 'multipurpose-ki7ln0/tinte-solo-puntas',
-			config: {
-				layout: 'month_view',
-			},
-		})
-	}
-
 	return (
 		<div className='flex flex-col items-center p-2 gap-2'>
 			<button
-				onClick={openCorte}
+				onClick={serviceCorte}
 				className='bg-blue-500 text-white p-2 rounded-md font-semibold text-md'
 			>
 				Corte clásico
 			</button>
 
 			<button
-				onClick={openCorteBarba}
+				onClick={serviceCorteBarba}
 				className='bg-blue-500 text-white p-2 rounded-md font-semibold text-md'
 			>
 				Corte + barba
 			</button>
 
 			<button
-				onClick={openSoloPuntas}
+				onClick={serviceSoloPuntas}
 				className='bg-blue-500 text-white p-2 rounded-md font-semibold text-md'
 			>
 				Tinte {`(solo puntas)`}
 			</button>
 
 			<button
-				onClick={openGlobal}
+				onClick={serviceGlobal}
 				className='bg-blue-500 text-white p-2 rounded-md font-semibold text-md'
 			>
 				Tinte Global
