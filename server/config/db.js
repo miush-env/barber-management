1262a024a2bcd400e9a7031ae2ff0bcee8f4ac26
Bau_ <basach1582@gmail.com>
2026-04-03 13:12:01 -0300

    implemente un comentario especial para que no se queje de que process no es reconocido

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
    try {
     await mongoose.connect(
+      // eslint-disable-next-line no-undef
       `mongodb+srv://multipurpose0509_db_user:${process.env.VITE_MONGO_PASSWORD}@barber-management.lwbg0t1.mongodb.net/barberDB`
     );
     console.log("🟢 MongoDB conectado funciona");
