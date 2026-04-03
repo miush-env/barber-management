d31d0ce67049113e6b815d8142570f19bcfbc01f
Bau_ <basach1582@gmail.com>
2026-04-03 13:21:13 -0300

    cree las rutas "/clerk" que muestra todos los usuarios logeados en clerk y luego la ruta "/relation" se encarga de ver si coincide el email de clerk con una colección de mongoDB

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/user.controller.js";
+import { createUser, getUsers, getUsersClerk, checkRelation } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.post("/", createUser);
 router.get("/", getUsers);
+
+router.get("/clerk", getUsersClerk);
+router.get("/relation", checkRelation);
 
 export default router;
