ed475f86335571899dbbf36de10f34788fd9571e
Bau_ <basach1582@gmail.com>
2026-04-05 16:00:15 -0300

    pido el nombre y email por prop saltándome el paso del mini formulario facilitando su uso para el usuario

--- src/utils/ServiceCal.js
+++ src/utils/ServiceCal.js
@@ -1,40 +1,40 @@
 import { getCalApi } from '@calcom/embed-react'
 const CALL_LINK = import.meta.env.VITE_CALL_LINK
 
-export const serviceCorte = async () => {
-		const cal = await getCalApi({ namespace: 'corte-clasico' })
-		cal('modal', {
-			calLink: `${CALL_LINK}/corte-clasico`,
-			config: {
-				layout: 'month_view',
-			},
-		})
-	}
+export const serviceCorte = async ({name, email}) => {
+	const cal = await getCalApi({ namespace: 'corte-clasico' })
+	cal('modal', {
+		calLink: `${CALL_LINK}/corte-clasico?name=${name}&email=${email}`,
+		config: {
+			layout: 'month_view',
+		},
+	})
+}
 
-export const serviceCorteBarba = async () => {
+export const serviceCorteBarba = async ({name, email}) => {
 		const cal = await getCalApi({ namespace: 'corte-clasico-barba' })
 		cal('modal', {
-			calLink: `${CALL_LINK}/corte-clasico-barba`,
+			calLink: `${CALL_LINK}/corte-clasico-barba?name=${name}&email=${email}`,
 			config: {
 				layout: 'month_view',
 			},
 		})
 	}
 
-export const serviceGlobal = async () => {
+export const serviceGlobal = async ({name, email}) => {
 		const cal = await getCalApi({ namespace: 'global' })
 		cal('modal', {
-			calLink: `${CALL_LINK}/global`,
+			calLink: `${CALL_LINK}/global?name=${name}&email=${email}`,
 			config: {
 				layout: 'month_view',
 			},
 		})
 	}
 
-export const serviceSoloPuntas = async () => {
+export const serviceSoloPuntas = async ({name, email}) => {
 		const cal = await getCalApi({ namespace: 'tinte-solo-puntas' })
 		cal('modal', {
-			calLink: `${CALL_LINK}/tinte-solo-puntas`,
+			calLink: `${CALL_LINK}/tinte-solo-puntas?name=${name}&email=${email}`,
 			config: {
 				layout: 'month_view',
 			},
