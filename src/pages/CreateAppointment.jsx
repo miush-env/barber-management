22ce5b33df367f69afdf7251bd5de7a036dcc5c0
Bau_ <basach1582@gmail.com>
2026-04-05 15:59:13 -0300

    cree una constante que almacena las funciones de calApi con un nombre que obligatoriamente debe coincidir con el slug del evento en caso de no coincidir llama a la funcion de servicioCorte luego implemente un map al estado de nameEvent renderizando cada servio y luego paso el objeto a ServiceCard

--- src/pages/CreateAppointment.jsx
+++ src/pages/CreateAppointment.jsx
@@ -1,10 +1,35 @@
-// import SectionTime from "../components/CreateAppointment/SectionTime"
 import NavBar from '../components/NavBar'
 import { useNavigate } from 'react-router'
-import Calendar from '@components/CreateAppointment/Cal.com/Calendar'
+import ServiceCard	from '@components/CreateAppointment/ServiceCard'
+import ButtonCallCal from '../components/CreateAppointment/Cal.com/ButtonCallCal'
+import { serviceCorte, serviceCorteBarba, serviceSoloPuntas, serviceGlobal } from '../utils/ServiceCal'
+import { getNameEvent } from '../utils/Bookings'
+import { useEffect, useState } from 'react'
 
 function CreateAppointment() {
-  	const navigate = useNavigate()
+	const [nameEvent, setNameEvent] = useState([])
+  const navigate = useNavigate()
+
+	const functionMapper = {
+		"corte-clasico": serviceCorte,
+		"corte-clasico-barba": serviceCorteBarba,
+		"tinte-solo-puntas": serviceSoloPuntas,
+		"global": serviceGlobal
+	};
+
+	useEffect(() => {
+		const fetchNameEvent = async () => {
+        try {
+            const res = await getNameEvent();
+            if (res) {
+              setNameEvent(res);
+            }
+        } catch (error) {
+          	console.error('Error en la petición:', error);
+        }
+    };
+    fetchNameEvent();
+	}, [])
 
 	return (
 		<main className='bg-gray-100 min-h-screen'>
@@ -34,7 +59,21 @@ function CreateAppointment() {
 			</header>
 
 			<div className='h-full bg-gray-100'>
-        <Calendar />
+				 <section className="mb-6" aria-labelledby="services-title">
+            <div className="flex flex-col gap-4 p-4">
+              {
+								nameEvent.map((s, index) => {
+									const calFunction = functionMapper[s.slug] || serviceCorte
+								
+									return (
+										<ButtonCallCal key={index} service={calFunction}>
+											<ServiceCard key={index} {...s} />
+										</ButtonCallCal>
+									)
+								})
+							}
+            </div>
+          </section>
 			</div>
 
 			<section className='fixed bottom-0 w-full h-14'>
