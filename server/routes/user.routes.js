636527cda07f82816197d2ec6fb208efe47b6066
Bau_ <basach1582@gmail.com>
2026-04-05 16:55:46 -0300

    cree la ruta donde obtendré las citas del usuario a traves de su clerkId

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createUser, getUsers, getUsersClerk, checkRelation } from "../controllers/user.controller.js";
+import { getUsers, getUsersClerk, checkRelation, getUserAppointment } from "../controllers/user.controller.js";
 import { checkOrCreateUser } from "../middlewares/checkOrCreateUser.js";
 
 const router = Router();
 
-router.post("/", createUser);
 router.get("/", getUsers);
 
 router.get("/clerk", getUsersClerk);
 router.post("/relation", checkOrCreateUser, checkRelation);
 
+router.get("/my-appointments", getUserAppointment)
 export default router;
