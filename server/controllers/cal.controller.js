d1539e5f81a0acefd3fd40f9a4fb00d0e4ed7b12
Bau_ <basach1582@gmail.com>
2026-04-08 17:48:37 -0300

    Cree una ruta básica para verificar si funcionaba y luego cree una funcion encargada de solicitar los datos a un webhook de cal que se activa al reservar una cita

---
+++ server/controllers/cal.controller.js
@@ -0,0 +1,36 @@
+export const getCalEndPoint = async (req, res) => {
+  try {
+    res.json({
+      mensaje: "ruta principal de cal"
+    });
+  // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    res.status(500).json({ error: "Error en la ruta" });
+  }
+}
+
+export const webHookCal = async (req, res) => {
+  const bookingData = req.body;
+
+	const getPriceFromDescription = (description) => {
+		const match = description.match(/\$(\d+)/);
+		return match ? Number(match[1]) : null;
+  };
+  const userAppointment = {
+    id: bookingData.payload.bookingId,
+    startTime: bookingData.payload.startTime,
+    status: bookingData.payload.status,
+    service: bookingData.payload.eventTitle,
+    price: getPriceFromDescription(bookingData.payload.eventDescription) 
+  }
+
+  try {  
+    // console.log(bookingData);
+    console.log(userAppointment);
+    res.status(200).json({ received: true });
+
+  } catch (error) {
+    console.error("Error al recibir el webhook:", error);
+    return res.status(500).json({ error: "Algo salió mal" });
+  }
+}
