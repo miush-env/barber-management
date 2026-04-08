a093223eda334dbcaa604e865ee01a6a2c7f8fa5
Bau_ <basach1582@gmail.com>
2026-04-08 17:53:51 -0300

    mejora de ui en el header y usando los iconos de lucide-react

--- src/components/panelBarber/Header.jsx
+++ src/components/panelBarber/Header.jsx
@@ -1,31 +1,40 @@
-function Header ({name, photo, notifications}) {
-  return (
+import { useUser } from '@clerk/react'
+import { Bell, BellRing } from 'lucide-react'
+
+function Header({ notifications }) {
+	const { user } = useUser()
+	const hour = new Date().toLocaleTimeString('es-ES', {
+		weekday: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+	})
+
+	return (
 		<header className='flex w-full items-center justify-between px-6 h-24 '>
 			<div className='flex gap-2 items-center'>
 				<img
-					src={photo}
-					alt={name}
-					className='rounded-full w-14 border-2 border-gray-400'
+					src={user.imageUrl}
+					alt={user.fullName}
+					className='rounded-full w-12 border-2 border-gray-400'
 				/>
 				<div className=''>
-					<span className='text-sm text-gray-600 font-semibold'>
-						07 vie Mar 2026
-					</span>
-					<h1 className='text-3xl text-black font-bold'>{name}</h1>
+						<h1 className='text-xl text-black font-bold'>{user.fullName}</h1>
+						<span className='text-sm text-gray-600 uppercase font-semibold'>{hour}</span>
 				</div>
 			</div>
 			<div className='relative'>
 				<span
-					className=' absolute -top-1 -right-1
+					className={`absolute -top-1 right-0
     flex items-center justify-center
-    w-5 h-5
+    w-3 h-3
     text-xs font-semibold
     text-white bg-red-600
-    rounded-full'
-				>
-					{notifications}
-				</span>
-				<img src='./src/assets/bell.svg' alt='icon bell' className='w-7' />
+    rounded-full ${notifications > 0 ? 'animate-pulse' : 'hidden'}`}
+				></span>
+				{
+					notifications > 0 ? <BellRing className='w-7s' /> : <Bell className='w-7' /> 
+				}
 			</div>
 		</header>
 	)
