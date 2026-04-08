b9abb2bb91a401adfb35d39e0d552e08d9b28564
Bau_ <basach1582@gmail.com>
2026-04-08 17:47:00 -0300

    cree el endpoint para llamar a un webHook de cal

--- server/App.js
+++ server/App.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.routes.js";
+import calRoutes from './routes/cal.routes.js'
 import { clerkMiddleware } from "@clerk/express";
 
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(clerkMiddleware());
 
 // rutas
 app.use("/api/users", userRoutes);
+app.use("/api/cal", calRoutes);
 
 // conectar DB
 connectDB();
