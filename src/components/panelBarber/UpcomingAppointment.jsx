f335124f006adc915be32e4f4f398052e972396a
Bau_ <basach1582@gmail.com>
2026-04-08 17:55:44 -0300

    Refactor el componente UpcomingAppointment para usar un array de citas y mejorar la legibilidad del código

--- src/components/panelBarber/UpcomingAppointment.jsx
+++ src/components/panelBarber/UpcomingAppointment.jsx
@@ -1,34 +1,50 @@
-import CardUpcomingAppointment from "./cards/CardUpcomingAppointment"
+import CardUpcomingAppointment from './cards/CardUpcomingAppointment'
 
 function UpcomingAppointment() {
-  return (
+
+	const appointments = [
+		{
+			id: 1,
+			clientName: 'Juan Ramirez',
+			time: '11:00',
+			service: 'Corte Clásico',
+			status: true,
+		},
+		{
+			id: 2,
+			clientName: 'Mateo Vazquez',
+			time: '12:00',
+			service: 'Degradado + Barba',
+			status: true,
+		},
+		{
+			id: 3,
+			clientName: 'Ariel Dundo',
+			time: '13:30',
+			service: 'Corte Clásico',
+			status: false,
+		},
+		{
+			id: 4,
+			clientName: 'Ariel Dundo',
+			time: '13:30',
+			service: 'Corte Clásico',
+			status: true,
+		},
+	]
+
+	return (
 		<article className='px-6 flex flex-col gap-2'>
 			<h2 className='text-xl text-black font-bold'>Próximas Citas</h2>
 			<div className='flex flex-col gap-4'>
-				<CardUpcomingAppointment
-					clientName='Juan Ramirez'
-					time='11:00'
-					service='Corte Clásico'
-					status={true}
-				/>
-				<CardUpcomingAppointment
-					clientName='Mateo Vazquez'
-					time='12:00'
-					service='Degradado + Barba'
-					status={true}
-				/>
-				<CardUpcomingAppointment
-					clientName='Ariel Dundo'
-					time='13:30'
-					service='Corte Clásico'
-					status={false}
-				/>
-				<CardUpcomingAppointment
-					clientName='Ariel Dundo'
-					time='13:30'
-					service='Corte Clásico'
-					status={true}
-				/>
+				{
+					appointments.map((client) => (
+						<CardUpcomingAppointment 
+							key={client.id}
+							appointment={client}
+						/>
+					))
+				}
 			</div>
 		</article>
 	)
