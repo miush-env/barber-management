56683e3251e2d4e9ea23f1242f105b6ae2bbb8f2
Bau_ <basach1582@gmail.com>
2026-04-28 16:32:17 -0300

    pase el objeto de la cita entero y lo paso a su componente hijo para la facilidad de manejos de datos futuros

--- src/components/panelBarber/NextAppointment.jsx
+++ src/components/panelBarber/NextAppointment.jsx
@@ -1,25 +1,14 @@
 import CardNextAppointment from './cards/CardNextAppointment'
 
 function NextAppointment({
-	nameClient,
-	status,
-	time,
-	serviceName,
-	timeService,
-	price,
+	appointment
 }) {
 	return (
 		<article className='p-6 flex flex-col gap-2'>
 			<h2 className='text-gray-600 uppercase text-sm font-bold'>Cita Actual</h2>
 			<div>
-				<CardNextAppointment
-					nameClient={nameClient}
-					status={status}
-					time={time}
-					serviceName={serviceName}
-					timeService={timeService}
-					price={price}
-				/>
+				{appointment === null ? <p className='text-gray-500'>No tienes citas programadas</p> : (	<CardNextAppointment appointment={appointment}/>)
+				}
 			</div>
 		</article>
 	)
