656bd110269f0e3e1033bfba3735ef566ff6c542
Bau_ <basach1582@gmail.com>
2026-06-06 15:22:25 -0300

    retoques finales y agregue un link para ir directo a ver todas las citas

--- src/components/panelBarber/UpcomingAppointment.jsx
+++ src/components/panelBarber/UpcomingAppointment.jsx
@@ -1,19 +1,24 @@
 import CardUpcomingAppointment from './cards/CardUpcomingAppointment'
+import { NavLink } from 'react-router'
 
 function UpcomingAppointment({ appointments }) {
-
 	return (
-		<article className='px-6 flex flex-col gap-2'>
-			<h2 className='text-xl text-black font-bold'>Próximas Citas</h2>
+		<article className='px-6 flex flex-col gap-3'>
+			<div className='flex justify-between items-center'>
+				<h2 className='text-gray-600 uppercase text-sm font-bold'>
+					Próximas Citas
+				</h2>
+
+				<NavLink to='/ver-citas' end>
+					<span className='text-sm text-blue-500 font-bold underline-offset-2 active:underline hover:underline'>
+						Ver todas
+					</span>
+				</NavLink>
+			</div>
 			<div className='flex flex-col gap-4'>
-				{
-					appointments.map((client) => (
-						<CardUpcomingAppointment 
-							key={client.id}
-							appointment={client}
-						/>
-					))
-				}
+				{appointments.map((client) => (
+					<CardUpcomingAppointment key={client.id} appointment={client} />
+				))}
 			</div>
 		</article>
 	)
