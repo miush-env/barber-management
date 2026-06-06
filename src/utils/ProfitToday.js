ff8748ff0b0611d2776c78babf9c1ca0d1c23d2d
Bau_ <basach1582@gmail.com>
2026-06-06 15:22:51 -0300

    retoque el nombre de las constante y nada mas

--- src/utils/ProfitToday.js
+++ src/utils/ProfitToday.js
@@ -1,14 +1,14 @@
-import { services } from "./services";
+import { Services } from './services'
 
 export const getProfitToday = (appointments) => {
-  let total = 0;
+	let total = 0
 
-  appointments.forEach((appointment) => {
-    const service = services[appointment.eventTypeId];
+	appointments.forEach((appointment) => {
+		const service = Services[appointment.eventTypeId]
 
-    if (service) {
-      total += service.price;
-    }
-  });
-  return total;
-};
+		if (service) {
+			total += service.price
+		}
+	})
+	return total
+}
--- src/utils/services.js
+++ src/utils/services.js
@@ -1,27 +1,32 @@
-export const services = {
-  5065863: {
-    id: 5065863,
-    name: "corte-clasico-barba",
-    price: 10000,
-  },
-  5065774: {
-    id: 5065774,
-    name: "corte-clasico",
-    price: 8000,
-  },
-  5065846: {
-    id: 5065846,
-    name: "barba",
-    price: 4000,
-  },
-  5065827: {
-    id: 5065827,
-    name: "tinte-solo-puntas",
-    price: 20000,
-  },
-  5065815: {
-    id: 5065815,
-    name: "global",
-    price: 25000,
-  },
-};
+export const Services = {
+	5065863: {
+		id: 5065863,
+		slug: 'corte-clasico-barba',
+		name: 'Corte Clasico + Barba',
+		price: 10000,
+	},
+	5065774: {
+		id: 5065774,
+		slug: 'corte-clasico',
+		name: 'Corte Clasico',
+		price: 8000,
+	},
+	5065846: {
+		id: 5065846,
+		slug: 'barba',
+		name: 'Barba',
+		price: 4000,
+	},
+	5065827: {
+		id: 5065827,
+		slug: 'tinte-solo-puntas',
+		name: 'Tinte Solo Puntas',
+		price: 20000,
+	},
+	5065815: {
+		id: 5065815,
+		slug: 'global',
+		name: 'Global',
+		price: 25000,
+	},
+}
