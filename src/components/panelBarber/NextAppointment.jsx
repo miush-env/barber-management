61b9c79c62f1b190d446736186f03e031f27e6c6
Bau_ <basach1582@gmail.com>
2026-06-06 15:21:43 -0300

    Ajustes menor en los estilos

--- src/components/panelBarber/NextAppointment.jsx
+++ src/components/panelBarber/NextAppointment.jsx
@@ -1,14 +1,15 @@
 import CardNextAppointment from './cards/CardNextAppointment'
 
-function NextAppointment({
-	appointment
-}) {
+function NextAppointment({ appointment }) {
 	return (
-		<article className='p-6 flex flex-col gap-2'>
+		<article className='p-6 flex flex-col gap-3'>
 			<h2 className='text-gray-600 uppercase text-sm font-bold'>Cita Actual</h2>
 			<div>
-				{appointment === null ? <p className='text-gray-500'>No tienes citas programadas</p> : (	<CardNextAppointment appointment={appointment}/>)
-				}
+				{appointment === null ? (
+					<p className='text-gray-500'>No tienes citas programadas</p>
+				) : (
+					<CardNextAppointment appointment={appointment} />
+				)}
 			</div>
 		</article>
 	)
